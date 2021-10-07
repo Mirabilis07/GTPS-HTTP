cccef7377e701d3980a34c9c70f6d1657d933345
Skys07 <74393073+Skys07@users.noreply.github.com>
2021-10-07 15:20:23 +0700

    Create http.js

---
+++ http.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const server = http.createServer(function (req, res) {
+    
+    if (req.url == "/growtopia/server_data.php") {
+        if (req.method === "POST") {
+        res.write('server|127.0.0.1\nport|17091\ntype|1\N#maint|Server is on maintenance.');
+        res.end();
+    }
+    else {
+        res.writeHead(200, { "Content-Type": "text/html" })
+        res.end();
+    }
+}
+});
+server.listen(80);
+console.log('HTTP server is up.')
